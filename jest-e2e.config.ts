566968642e50f525eba73ffcd9af53e67a6ef744
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-24 11:13:11 -0300

    UNIT TEST for envconfigservice

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -6,7 +6,7 @@ export default {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testRegex: '.*\\.spec\\.ts$',
+  testRegex: '.*\\.e2e-spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
