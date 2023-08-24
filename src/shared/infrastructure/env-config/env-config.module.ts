f1fc984fa5a1b647191b96d0e0d2b391b1845646
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-24 13:48:27 -0300

    Creating setters in user entity

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,9 +1,15 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
-import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import {
+  ConfigModule,
+  ConfigModuleOptions,
+  ConfigService,
+} from '@nestjs/config';
 import { join } from 'node:path';
 @Module({
-  providers: [EnvConfigService],
+  providers: [EnvConfigService, ConfigService],
+
+  exports: [EnvConfigService, ConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -15,13 +15,31 @@ export class UserEntity extends BaseEntity<UserProps> {
   get name() {
     return this.props.name;
   }
+
+  private set name(value: string) {
+    this.props.name = value;
+  }
+
   get email() {
     return this.props.email;
   }
+
   get password() {
     return this.props.password;
   }
+
+  private set password(value: string) {
+    this.props.password = value;
+  }
+
   get createdAt() {
     return this.props.createdAt;
+  }
+
+  update(value: string): void {
+    this.name = value;
+  }
+  updatePassword(value: string): void {
+    this.password = value;
   }
 }
