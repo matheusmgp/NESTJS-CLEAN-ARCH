8ac78f8215a14acf6a45cf0751808c59608396aa
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-26 13:03:23 -0300

    SearchResult

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -2,12 +2,22 @@ import { BaseEntity } from '../entities/entity';
 import { IRepository } from './repository-contracts';
 
 export type SortDirection = 'asc' | 'desc';
+
 export type SearchProps<Filter = string> = {
   page?: number;
   perPage?: number;
   sort?: string | null;
   sortDir?: SortDirection | null;
   filter?: Filter;
+};
+export type SearchResultProps<E extends BaseEntity, Filter> = {
+  items: E[];
+  total: number;
+  currentPage: number;
+  perPage: number;
+  sort: string | null;
+  sortDir: string | null;
+  filter: Filter | null;
 };
 
 export class SearchParams {
@@ -77,6 +87,38 @@ export class SearchParams {
     )
       _perPage = 15;
     return _perPage;
+  }
+}
+export class SearchResult<E extends BaseEntity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly currentPage: number;
+  readonly perPage: number;
+  readonly lastPage: number;
+  readonly sort: string | null;
+  readonly sortDir: string | null;
+  readonly filter: Filter | null;
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.currentPage = props.currentPage;
+    this.perPage = props.perPage;
+    this.lastPage = Math.ceil(this.total / this.perPage);
+    this.sort = props.sort ?? null;
+    this.sortDir = props.sortDir ?? null;
+    this.filter = props.filter ?? null;
+  }
+  toJSON(forceEntity = false) {
+    return {
+      items: forceEntity ? this.items.map(item => item.toJSON()) : this.items,
+      total: this.total,
+      currentPage: this.currentPage,
+      perPage: this.perPage,
+      lastPage: this.lastPage,
+      sort: this.sort,
+      sortDir: this.sortDir,
+      filter: this.filter,
+    };
   }
 }
 export interface ISearchableRepository<
