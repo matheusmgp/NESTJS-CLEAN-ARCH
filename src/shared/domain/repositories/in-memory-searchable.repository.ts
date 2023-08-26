48615cd099ba67741656a0df737fddb050eee775
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-26 14:22:10 -0300

    InMemorySearchableRepository

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,52 @@
 import { BaseEntity } from '../entities/entity';
 import { InMemoryRepository } from './in-memory.repository';
-import { ISearchableRepository } from './searchable-repository-contracts';
+import {
+  ISearchableRepository,
+  SearchParams,
+  SearchResult,
+} from './searchable-repository-contracts';
 
 export abstract class InMemorySearchableRepository<E extends BaseEntity>
   extends InMemoryRepository<E>
   implements ISearchableRepository<E, any, any>
 {
-  search(props: any): Promise<any> {
-    throw new Error('Method not implemented.');
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = await this.applySort(
+      this.items,
+      props.sort,
+      props.sortDir,
+    );
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.perPage,
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: props.sort,
+      sortDir: props.sortDir,
+      filter: props.filter,
+    });
   }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {}
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {}
 }
