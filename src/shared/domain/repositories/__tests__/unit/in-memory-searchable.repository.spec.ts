68e0c71a7526c4eb4dd95e72f3e438b8a67966e4
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-26 15:55:39 -0300

    applyPaginate

--- src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory-searchable.repository.spec.ts
@@ -79,7 +79,21 @@ describe('InMemorySearchable Repository unit tests', () => {
     });
   });
   describe('applyPaginate method', () => {
-    it('', () => {});
+    it('should paginate items', async () => {
+      const items = [
+        new StubEntity({ name: 'a', price: 100 }),
+        new StubEntity({ name: 'b', price: 100 }),
+        new StubEntity({ name: 'c', price: 100 }),
+        new StubEntity({ name: 'd', price: 100 }),
+        new StubEntity({ name: 'e', price: 100 }),
+      ];
+      let itemsPaginated = await sut['applyPaginate'](items, 1, 2);
+      expect(itemsPaginated).toStrictEqual([items[0], items[1]]);
+      itemsPaginated = await sut['applyPaginate'](items, 2, 2);
+      expect(itemsPaginated).toStrictEqual([items[2], items[3]]);
+      itemsPaginated = await sut['applyPaginate'](items, 3, 2);
+      expect(itemsPaginated).toStrictEqual([items[4]]);
+    });
   });
   describe('search method', () => {
     it('', () => {});
