0ef619fda23ed710a019c794d1fa7059caf9ec5d
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-28 16:10:43 -0300

    hashing password before instantiating userentity

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { IUserRepository } from '@/users/domain/repositories/user.repository';
 import { BadRequestError } from '../errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
+import { IHashProvider } from '@/shared/application/providers/hash-provider';
 
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace SignupUseCase {
@@ -17,15 +18,23 @@ export namespace SignupUseCase {
     createdAt: Date;
   };
   export class UseCase {
-    constructor(private readonly userReporitoy: IUserRepository.Repository) {}
+    constructor(
+      private readonly userReporitoy: IUserRepository.Repository,
+      private readonly bcryptProvider: IHashProvider,
+    ) {}
     async execute(input: Input): Promise<Output> {
       const { name, password, email } = input;
       if (!name || !password || !email)
         throw new BadRequestError(`Input data not provided`);
 
       await this.userReporitoy.emailExists(email);
+      const hashPassword = await this.bcryptProvider.generateHash(password);
 
-      const entity = new UserEntity(input);
+      const entity = new UserEntity(
+        Object.assign(input, {
+          password: hashPassword,
+        }),
+      );
 
       await this.userReporitoy.insert(entity);
 
