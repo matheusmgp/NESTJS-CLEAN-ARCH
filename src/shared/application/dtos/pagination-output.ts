42f07d1ddcd40271621bd2568cf7f4981b02a701
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-08-29 07:57:15 -0300

    UserOutputMapper

---
+++ src/shared/application/dtos/pagination-output.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from '@/shared/domain/entities/entity';
+import { SearchResult } from '@/shared/domain/repositories/searchable-repository-contracts';
+
+export type PaginationOutput<Item = any> = {
+  items: Item[];
+  total: number;
+  currentPage: number;
+  lastPage: number;
+  perPage: number;
+};
+
+export class PaginationOutputMapper {
+  static toOutput<Item = any>(
+    items: Item[],
+    result: SearchResult<BaseEntity>,
+  ): PaginationOutput<Item> {
+    return {
+      items,
+      total: result.total,
+      currentPage: result.currentPage,
+      lastPage: result.lastPage,
+      perPage: result.perPage,
+    };
+  }
+}
--- src/users/application/usecases/get-users-list.usecase.ts
+++ src/users/application/usecases/get-users-list.usecase.ts
@@ -1,16 +1,28 @@
 import { IUserRepository } from '@/users/domain/repositories/user.repository';
 import { IUseCase } from '@/shared/application/usecases/use-case';
 import { SearchInput } from '@/shared/application/dtos/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
-  export type Output = void;
+  export type Output = PaginationOutput<UserOutput>;
   export class UseCase implements IUseCase<Input, Output> {
     constructor(private readonly userRepository: IUserRepository.Repository) {}
     async execute(input: Input): Promise<Output> {
       const params = new IUserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: IUserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
