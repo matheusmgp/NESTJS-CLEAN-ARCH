f2021c629c2272cf1f23c2abd0d30ec6279b41df
Matheus Gustavo <matheus_mgp@hotmail.com>
2023-09-01 08:58:41 -0300

    Config e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/infrastructure/users.module';
 import { DatabaseModule } from './shared/infrastructure/database/database.module';
 
 @Module({
   imports: [EnvConfigModule, UsersModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
